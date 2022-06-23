f67db84d185b8fc4b55bd3a4f23b67c5d9599b29
RisedNote <risednotes@gmail.com>
2022-06-22 17:14:34 -0700

    adding to search script

--- newtabPage/sreachScript.js
+++ newtabPage/sreachScript.js
@@ -1,41 +1,46 @@
 // grabs all links from the shortcuts
 
-let linkArray = [];
+const uiNames = document.querySelectorAll("a[href]");
+const uiNamesarr = Array.from(uiNames);
 
-document.querySelectorAll("li > a").forEach(getLinks);
-function getLinks(item) {
-  linkArray.push(item.href); 
-}
+let objOfLinks = [];
+objOfLinks = uiNamesarr.map((item) => {
+	return {name: item.firstChild.data, link: item.attributes.href.nodeValue};
+});
 
-let nameArray = [];
-
-for (let i = 0; i < document.querySelectorAll("li > a").length; i++) {
-  nameArray.push(document.querySelectorAll("li > a")[i].innerHTML);
-}
+let arrOfNames = []
+objOfLinks.forEach((e) => {
+  
+  arrOfNames.push(e.name);
+})
 
 // if the input search has a similar pattern it takes you to that website 
 document.querySelector(".search-form").addEventListener( "keydown", function( e ) { 
-    let keyCode = e.keyCode || e.which; // runs when user presses "enter" key
-    if ( keyCode === 13 ) {
-      const userInput = document.querySelector(".search-form").value; // grabs what user typed
+  let keyCode = e.keyCode || e.which; // runs when user presses "enter" key
+  if ( keyCode === 13 ) {
+    const userInput = document.querySelector(".search-form").value; // grabs what user typed
 
-      const namematch = nameArray.find(element => {
-        if (element.includes(userInput)) { // checks if user input is a substring of any in the "linkArray"
-          return true;
-        }
-      });
-
-      const linkmatch = linkArray.find(element => {
-        if (element.includes(namematch)) {
-          return true;
-        }
-      });
-
-      if (linkmatch) {
-      window.open(linkmatch, '_self'); // website, replace '_self' with '_blank' if wanting to open a newtab instead
-      } else {
-      const link = "https://search.brave.com/search?q=".concat(...userInput); // if does match with any shortcut goes to brave to search
-      window.open(link, '_self');
+    const match = arrOfNames.find(name => {
+      if (name.toLowerCase().substring(0, 3).includes(userInput.toLowerCase())) { // checks if user input is a substring of 3 in the "nameArray"
+        return true;
       }
+    });
+
+    //console.log(match);
+
+    let linkToOpen = false;
+    objOfLinks.forEach((e) => {
+      if (match === e.name) {
+        linkToOpen = e.link;
+      } 
+    });
+    if (linkToOpen) {
+      window.open(linkToOpen, "_self");
+    } else {
+      const baseLink = "https://search.brave.com/search?q=";
+      const link = baseLink.concat(userInput);
+      console.log(link);
+      window.open(link, "_self");
     }
+  }
 }, false);
